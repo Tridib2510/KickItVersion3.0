32f8f9995112521eb41cbc9fdf8d3de80172c051
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2026-05-15 21:24:40 +0530

    remove chat icon

--- Client/src/Components/Navbar/Navbar.tsx
+++ Client/src/Components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Menu, X, MessageCircle, Home, Calendar } from "lucide-react";
+import { Menu, X, Home, Calendar } from "lucide-react";
 import { useAuthStore } from "../../store/Auth";
 import clsx from "clsx";
 import LoginPopup from "../Login/Login";
@@ -89,12 +89,6 @@ const Navbar: React.FC = () => {
             {isLoggedIn ? (
               <div className="flex items-center gap-3 ml-4 pl-4 border-l border-gray-700">
                 <NotificationDropdown />
-                <Link
-                  to="/chat"
-                  className="p-2 rounded-xl text-gray-300 hover:text-white hover:bg-white/10 transition-all"
-                >
-                  <MessageCircle className="w-5 h-5" />
-                </Link>
                 <Link to="/profile" className="focus:outline-none">
                   <img
                     src={profile}
